6d8633161920f5349f12a6d1b7c535c5d4ee8ee1
Stephan Seliuk <stepan@selyuk.com>
2018-05-24 23:21:59 +0300

    Added labels, ports, mem_limit, cpu_shares, hostname

--- docker-compose-gen.sh
+++ docker-compose-gen.sh
@@ -4,10 +4,17 @@ docker-yml() {
   docker inspect -f $'
 {{.Name}}
   image: {{.Config.Image}}
+  hostname: {{.Config.Hostname}}
   entrypoint: {{json .Config.Entrypoint}}
+  mem_limit: {{.HostConfig.Memory}}
+  cpu_shares: {{.HostConfig.CpuShares}}
+  ports: {{range $p, $conf := .NetworkSettings.Ports}}
+    - {{(index $conf 0).HostIp}}:{{(index $conf 0).HostPort}}:{{$p}}{{end}}
   environment: {{range .Config.Env}}
     - {{.}}{{end}}
   volumes: {{range .Config.Volumes}}
+    - {{.}}{{end}}
+  labels: {{range .Config.Labels}}
     - {{.}}{{end}}
 ' $1
 }
