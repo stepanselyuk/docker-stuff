4b31afda0f31e903bfc76d40f35c004f61440f78
Stephan Seliuk <stepan@selyuk.com>
2018-05-24 22:55:17 +0300

    Initial commit

---
+++ docker-compose-gen.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+docker-yml() {
+  docker inspect -f $'
+{{.Name}}
+  image: {{.Config.Image}}
+  entrypoint: {{json .Config.Entrypoint}}
+  environment: {{range .Config.Env}}
+    - {{.}}{{end}}
+' $1
+}
+
+for i in $( docker ps --format "{{.ID}}" ); do
+   docker-yml() $i
+done
