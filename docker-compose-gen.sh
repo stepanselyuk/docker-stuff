fda43bb9fa848b9d01c8e7a6a178edba649e6d3b
Stephan Seliuk <stepan@selyuk.com>
2018-05-25 23:53:34 +0300

    An update

--- docker-compose-gen.sh
+++ docker-compose-gen.sh
@@ -2,28 +2,31 @@
 
 docker-yml() {
   /usr/bin/docker inspect -f $'
-{{.Name}}
-  image: {{.Config.Image}}
-  hostname: {{.Config.Hostname}}
-  entrypoint: {{json .Config.Entrypoint}}
-  mem_limit: {{printf "%.f" .HostConfig.Memory}}
-  cpu_shares: {{.HostConfig.CpuShares}}
-  {{- if .NetworkSettings.Ports}}
-  ports: {{range $p, $conf := .NetworkSettings.Ports}}
-    - {{(index $conf 0).HostIp}}:{{(index $conf 0).HostPort}}:{{$p}}{{end}}
-  {{- end}}
-  {{- if .Config.Env}}
-  environment: {{range .Config.Env}}
-    - {{.}}{{end}}
-  {{- end}}
-  {{- if .Config.Volumes}}
-  volumes: {{range .Config.Volumes}}
-    - {{.}}{{end}}
-  {{- end}}
-  {{- if .Config.Labels}}
-  labels: {{range $n, $v := .Config.Labels}}
-    - {{$n}}: {{$v}}{{end}}
-  {{- end}}
+version: "2.3"
+services:
+    {{.Name}}:
+        image: {{.Config.Image}}
+        container_name: {{.Name}}
+        hostname: {{.Config.Hostname}}
+        entrypoint: {{json .Config.Entrypoint}}
+        mem_limit: {{printf "%.f" .HostConfig.Memory}}
+        cpu_shares: {{.HostConfig.CpuShares}}
+        {{- if .NetworkSettings.Ports}}
+        ports: {{range $p, $conf := .NetworkSettings.Ports}}
+        - {{(index $conf 0).HostIp}}:{{(index $conf 0).HostPort}}:{{$p}}{{end}}
+        {{- end}}
+        {{- if .Config.Env}}
+        environment: {{range .Config.Env}}
+        - {{.}}{{end}}
+        {{- end}}
+        {{- if .Config.Volumes}}
+        volumes: {{range .Config.Volumes}}
+        - {{.}}{{end}}
+        {{- end}}
+        {{- if .Config.Labels}}
+        labels: {{range $n, $v := .Config.Labels}}
+        - {{$n}}: {{$v}}{{end}}
+        {{- end}}
 ' $1
 }
 
