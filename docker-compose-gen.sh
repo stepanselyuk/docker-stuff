ed043c295ae7020a429bf52cecbf2e0eb95c78e4
Stephan Seliuk <stepan@selyuk.com>
2018-05-28 22:42:56 +0300

    An update

--- docker-compose-gen.sh
+++ docker-compose-gen.sh
@@ -14,7 +14,8 @@ docker-yml() {
         cpu_shares: {{.HostConfig.CpuShares}}
         {{- if .NetworkSettings.Ports}}
         ports: {{range $p, $conf := .NetworkSettings.Ports}}
-        {{- if $conf}}- {{(index $conf 0).HostIp}}:{{(index $conf 0).HostPort}}:{{$p}}{{- end}}{{end}}
+        {{- if $conf}}
+        - {{(index $conf 0).HostIp}}:{{(index $conf 0).HostPort}}:{{$p}}{{- end}}{{end}}
         {{- end}}
         {{- if .Config.Env}}
         environment: {{range .Config.Env}}
