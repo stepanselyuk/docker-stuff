582fc29a7e91b71d73027726e36ee991a6d4e50f
Stephan Seliuk <stepan@selyuk.com>
2018-06-02 22:35:13 +0300

    An update

--- docker-compose-gen.sh
+++ docker-compose-gen.sh
@@ -5,7 +5,7 @@ echo "services:"
 
 docker-yml() {
   /usr/bin/docker inspect -f $'
-    {{.Name}}:
+    {{print .Name[1:]}}:
         image: {{.Config.Image}}
         container_name: {{.Name}}
         hostname: {{.Config.Hostname}}
